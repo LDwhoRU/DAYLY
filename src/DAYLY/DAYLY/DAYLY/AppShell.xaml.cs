f783b51dac6bf7d4e5e939ae00eba83c31f32992
Alex Lossberg <50721703+LDwhoRU@users.noreply.github.com>
2020-09-07 11:03:44 +1000

    Init code base

---
+++ src/DAYLY/DAYLY/DAYLY/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using DAYLY.ViewModels;
+using DAYLY.Views;
+using Xamarin.Forms;
+
+namespace DAYLY
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ src/DAYLY/DAYLY/DAYLY/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using DAYLY.ViewModels;
+
+namespace DAYLY.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
