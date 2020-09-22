6825d2c51b28712758337e406b05367831d899ae
Ethan-gif <62533979+Ethan-gif@users.noreply.github.com>
2020-09-22 13:34:22 +1000

    alert page ui
    added images for the buttons on the alert page. Also installed a nuget package for the monthly view

---
+++ src/DAYLY/DAYLY/DAYLY/Views/alert.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DAYLY.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class alert : ContentPage
+    {
+        public alert()
+        {
+            InitializeComponent();
+        }
+    }
+}
