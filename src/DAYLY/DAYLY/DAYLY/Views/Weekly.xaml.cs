989867a57941a333350769968ea1a9bf74b4d6a0
n10255419 <n10255419@qut.edu.au>
2020-09-23 09:56:54 +1000

    Naming fix on weekly page
    Fixed naming on weekly page

--- src/DAYLY/DAYLY/DAYLY/Views/Weekly.xaml.cs
+++ src/DAYLY/DAYLY/DAYLY/Views/Weekly.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace DAYLY.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page1 : ContentPage
+    public partial class Weekly : ContentPage
     {
-        public Page1()
+        public Weekly()
         {
             InitializeComponent();
         }
