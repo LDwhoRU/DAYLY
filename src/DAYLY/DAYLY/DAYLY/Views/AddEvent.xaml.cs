60c3c90fdae22197add84ef0efc80a6d00463acc
Alex Lossberg <50721703+LDwhoRU@users.noreply.github.com>
2020-09-24 11:14:29 +1000

    Renamed class

--- src/DAYLY/DAYLY/DAYLY/Views/AddEvent.xaml.cs
+++ src/DAYLY/DAYLY/DAYLY/Views/AddEvent.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace DAYLY.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page1 : ContentPage
+    public partial class AddEvent : ContentPage
     {
-        public Page1()
+        public AddEvent()
         {
             InitializeComponent();
         }
