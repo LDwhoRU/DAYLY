3cf51d801329138407810747da886940d6ee3aab
Alex Lossberg <50721703+LDwhoRU@users.noreply.github.com>
2020-09-30 04:52:32 +1000

    Added require model fields for Event implementation

---
+++ src/DAYLY/DAYLY/DAYLY/Models/Affair.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DAYLY.Models
+{
+    public class Affair
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public int RepeatInterval { get; set; }
+        public int AlertInterval { get; set; }
+        public Note NoteEntry { get; set; }
+        public Programme SelectedProgramme { get; set; }
+    }
+}
