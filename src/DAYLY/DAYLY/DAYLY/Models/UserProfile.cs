6aa6f3ed6a6e8db9eb5c0ae1b760935cdd2cecfc
duylhng <63448508+duylhng@users.noreply.github.com>
2020-10-14 10:24:44 +1000

    Created model for user profile

---
+++ src/DAYLY/DAYLY/DAYLY/Models/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DAYLY.Models
+{
+    public class UserProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TasksRemaining { get; set; }
+        public string TasksCompleted { get; set; }
+        public string Organisation { get; set; }
+        public string Course { get; set; }
+    }
+}
