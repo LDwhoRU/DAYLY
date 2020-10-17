5348db41e8afef13e18f217fe189a83487c8c3c5
Ethan-gif <62533979+Ethan-gif@users.noreply.github.com>
2020-10-17 14:51:27 +1000

    started mock data
    started making the mock data class for events. Changed variables in event from timespan to datetime as I think it makes more logical sense that way

--- src/DAYLY/DAYLY/DAYLY/Models/Event.cs
+++ src/DAYLY/DAYLY/DAYLY/Models/Event.cs
@@ -4,10 +4,11 @@ namespace DAYLY.Models
 {
     public class Event : Affair
     {
+        
         public bool IsOnline { get; set; }
         public bool AllDay { get; set; }
-        public TimeSpan StartTime { get; set; }
-        public TimeSpan EndTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
         public string Location { get; set; }
     }
 }
---
+++ src/DAYLY/DAYLY/DAYLY/Services/MockEventData.cs
@@ -0,0 +1,51 @@
+﻿using DAYLY.Models;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAYLY.Services
+{
+    public class MockEventData : IDataStore<Event>
+    {
+        readonly List<Event> events;
+
+        public MockEventData()
+        {
+            DateTime datetime1 = new DateTime(2020, 10, 17, 8, 30, 0);
+            DateTime datetime2 = new DateTime(2020, 10, 17, 9, 30, 0);
+
+            events = new List<Event>()
+
+            {
+                new Event { Id = "1", Name = "CAB303", Type = "tute", Date = datetime1, RepeatInterval = 1, AlertInterval=1, NoteEntry=null,SelectedProgramme=null,AllDay=false,StartTime=datetime1,EndTime=datetime2,Location="P5" },
+     };
+        }
+        public async Task<bool> AddItemAsync(Event item)
+        {
+            events.Add(item);
+            return await Task.FromResult(true);
+        }
+
+        public Task<bool> DeleteItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Event> GetItemAsync(string id)
+        {
+            return await Task.FromResult(events.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<Event>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(events);
+        }
+
+        public Task<bool> UpdateItemAsync(Event item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
