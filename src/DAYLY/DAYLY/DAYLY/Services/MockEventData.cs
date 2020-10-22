b68ed19d4305b0c65f6bc02f607ee9565a30d825
Ethan-gif <62533979+Ethan-gif@users.noreply.github.com>
2020-10-22 13:26:03 +1000

    monthly view working
    shows correct dates and names and notes just need to add colours

--- src/DAYLY/DAYLY/DAYLY/Services/MockEventData.cs
+++ src/DAYLY/DAYLY/DAYLY/Services/MockEventData.cs
@@ -4,25 +4,33 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using DAYLY.Views;
 
 namespace DAYLY.Services
 {
     public class MockEventData : IDataStore<Event>
     {
+      //  public List<Note> Notess { get; }
+
         readonly List<Event> events;
+        readonly List<Note> Notess;
 
         public MockEventData()
         {
+            Note testnot = new Note();
+            testnot.Id = 1;
+            testnot.URL = null;
+            testnot.Description = "this is my description epic nice";
             Programme red = new Programme();
             red.HexColour = "green";
             Programme pink = new Programme();
             pink.HexColour = "pink";
-            DateTime datetime1 = new DateTime(2020, 10, 19, 8, 30, 0);
-            DateTime datetime2 = new DateTime(2020, 10, 21, 9, 30, 0);
+            DateTime datetime1 = new DateTime(2020, 11, 19, 8, 30, 0);
+            DateTime datetime2 = new DateTime(2020, 11, 21, 9, 30, 0);
             DateTime datetime3 = new DateTime(2020, 10, 23, 9, 30, 0);
-            DateTime datetime4 = new DateTime(2020, 10, 18, 9, 30, 0);
+            DateTime datetime4 = new DateTime(2020, 9, 18, 9, 30, 0);
             DateTime datetime5 = new DateTime(2020, 10, 20, 9, 30, 0);
-            DateTime datetime6 = new DateTime(2020, 10, 22, 9, 30, 0);
+            DateTime datetime6 = new DateTime(2020, 9, 22, 9, 30, 0);
             DateTime datetime7 = new DateTime(2020, 10, 24, 9, 30, 0);
             TimeSpan span = new TimeSpan(7, 0, 0);
             TimeSpan span3 = new TimeSpan(12, 0, 0);
@@ -30,19 +38,22 @@ namespace DAYLY.Services
             TimeSpan span4 = new TimeSpan(11, 0, 0);
             TimeSpan span5 = new TimeSpan(16, 0, 0);
             TimeSpan span6 = new TimeSpan(18, 0, 0);
-
+            Notess = new List<Note>() {
+               new Note{Id=1,URL=null,Description="testing this epic dude stuff my guy"},
+               new Note{Id=2,URL=null,Description="testing number 2 shmole"},
+           };
 
 
             events = new List<Event>()
 
             {
-                new Event { Id = 1, Name = "Monday test", Type = "tute", Date = datetime1, RepeatInterval = 1, AlertInterval=1,NoteId=1 ,ProgrammeId=1,AllDay=false,StartTime=span, EndTime=span3,Location="P5" },
-                 new Event { Id = 3, Name = "2nd monday", Type = "tute", Date = datetime1, RepeatInterval = 1, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span2, EndTime=span6,Location="P5" },
+                new Event { Id = 1, Name = "Monday test", Type = "tute", Date = datetime1, RepeatInterval = 1, AlertInterval=1,NoteId=2 ,ProgrammeId=1,AllDay=false,StartTime=span, EndTime=span3,Location="P5" },
+                 new Event { Id = 3, Name = "2nd monday", Type = "tute", Date = datetime1, RepeatInterval = 2, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span2, EndTime=span6,Location="P5" },
                 //new Event { Id = "2", Name = "CAB303", Type = "tute", Date = datetime1, RepeatInterval = 1, AlertInterval=1, NoteEntry=null,SelectedProgramme=pink,AllDay=false,StartTime=span5, EndTime=span6,Location="P5" },
-                // new Event { Id = 4, Name = "wednesday dude", Type = "tute", Date = datetime2, RepeatInterval = 1, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span4, EndTime=span3,Location="P5" },
-              //   new Event { Id = 5, Name = "friyayayayayayay", Type = "tute", Date = datetime3, RepeatInterval = 1, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span5, EndTime=span6,Location="P5" },
-                 //   new Event { Id = 6, Name = "sunday type beat", Type = "tute", Date = datetime4, RepeatInterval = 1, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span, EndTime=span2,Location="P5" },
-                //     new Event { Id = 6, Name = "tuesdeee", Type = "tute", Date = datetime5, RepeatInterval = 1, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span3, EndTime=span2,Location="P5" },
+                 new Event { Id = 4, Name = "wednesday dude", Type = "tute", Date = datetime2, RepeatInterval = 1, AlertInterval=1, NoteId=2,ProgrammeId=1,AllDay=false,StartTime=span4, EndTime=span3,Location="P5" },
+                 new Event { Id = 5, Name = "friyayayayayayay", Type = "tute", Date = datetime3, RepeatInterval = 2, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span5, EndTime=span6,Location="P5" },
+                    new Event { Id = 6, Name = "sunday type beat", Type = "tute", Date = datetime4, RepeatInterval = 1, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span, EndTime=span2,Location="P5" },
+                     new Event { Id = 6, Name = "tuesdeee", Type = "tute", Date = datetime5, RepeatInterval = 1, AlertInterval=1, NoteId=2,ProgrammeId=1,AllDay=false,StartTime=span3, EndTime=span2,Location="P5" },
                       new Event { Id = 6, Name = "thursssbrahhh", Type = "tute", Date = datetime6, RepeatInterval = 1, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span4, EndTime=span5,Location="P5" },
                        new Event { Id = 6, Name = "satatatatatata", Type = "tute", Date = datetime7, RepeatInterval = 1, AlertInterval=1, NoteId=1,ProgrammeId=1,AllDay=false,StartTime=span4, EndTime=span6,Location="P5" },
      };
@@ -67,6 +78,10 @@ namespace DAYLY.Services
         public async Task<IEnumerable<Event>> GetItemsAsync(bool forceRefresh = false)
         {
             return await Task.FromResult(events);
+        }
+        public async Task<IEnumerable<Note>> GetNotesAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(Notess);
         }
 
         public Task<bool> UpdateItemAsync(Event item)
--- src/DAYLY/DAYLY/DAYLY/ViewModels/MonthlyViewModel.cs
+++ src/DAYLY/DAYLY/DAYLY/ViewModels/MonthlyViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using DAYLY.Models;
+using DAYLY.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Plugin.Calendar.Models;
@@ -11,47 +13,80 @@ namespace DAYLY.ViewModels
 {
     class MonthlyViewModel : BaseViewModel
     {
+        private List<Event> zz;
+
         public EventCollection Events { get; set; }
           public ObservableCollection<Event> Eventz { get; }
+        public ObservableCollection<Note> notes { get; }
         public MonthlyViewModel()
         {
             Task.Run(async () => await ExecuteLoadItemsCommand());
-
+           
             Eventz = new ObservableCollection<Event>();
-            Console.WriteLine("below");
-            Console.WriteLine(Eventz[1].Date);
-            Events = new EventCollection
-            
-            //Events = Eventz;
-            {
-           //    [Eventz[1].Date] = new List<Event>
-  //  {
-       // new Event { Name = Eventz[1].Name, Type = "This is Cool event1's description!" },
-      //  new Event { Name = Eventz[1].Name,  Type = "This is Cool event2's description!" }
-  //  },
-                // 5 days from today
-         //       [Eventz[0].Date] = new List<Event>
-   // {
-   //  //   new Event { Name = Eventz[0].Name,  Type = "This is Cool event3's description!" },
-     //   new Event { Name = Eventz[0].Name,  Type = "This is Cool event4's description!" }
-   // },
-                // 3 days ago
-          //      [Eventz[2].Date] = new List<Event>
-  //  {
-    //    new Event { Name = Eventz[2].Name, Type = "This is Cool event5's description!" }
-  //  },
-                // custom date
-         //       [Eventz[3].Date] = new List<Event>
-  //  {
-       // new Event { Name = Eventz[3].Name,  Type = "This is Cool event6's description!" }
-   // }
-            };
-            List<Event> zz = new List<Event>();
-            zz.Add(Eventz[1]);
-            Events.Add((Eventz[1].Date), zz);
-          
-        //    Task.Run(async () => await ExecuteLoadItemsCommand());
+            //   Console.WriteLine("below");
+            //   Console.WriteLine(Eventz[1].Date);
+            notes = new ObservableCollection<Note>();
+            Events = new EventCollection();
+            Task.Run(async () => await Populate());
+
         }
+          async Task Populate()
+        {
+            IsBusy = true;
+         
+                try {
+           
+                Console.WriteLine(Eventz.ToString());
+                int index = new int();
+                for (int i = 0; i < Eventz.Count; i++) {
+                    Console.WriteLine("below me");
+                    zz = new List<Event>();
+                    foreach (var x in notes)
+                    {
+                        if (x.Id == Eventz[i].NoteId)
+                        {
+                            index = notes.IndexOf(x);
+                        }
+                    }
+                    var temp = new List<Event>(Eventz);
+                    zz.Add(new Event { Name =temp[i].Name, Type = notes[index].Description });
+                    foreach (var eventt in temp) {
+                        if (eventt.Date == temp[i].Date&&eventt.Name!=temp[i].Name) {
+                            foreach (var x in notes)
+                            {
+                                if (x.Id == eventt.NoteId)
+                                {
+                                    index = notes.IndexOf(x);
+                                }
+                            }
+                            zz.Add(new Event { Name = eventt.Name, Type = notes[index].Description });
+                            Console.WriteLine(eventt.Name);
+                            Eventz.Remove(eventt);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    Console.WriteLine(zz[0].Name);
+                    Events.Add(Eventz[i].Date,zz);
+                }
+           
+             
+                }
+
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+            }
+        
+        //    Task.Run(async () => await ExecuteLoadItemsCommand());
+        
         async Task ExecuteLoadItemsCommand()
         {
             IsBusy = true;
@@ -64,10 +99,13 @@ namespace DAYLY.ViewModels
                 {
                     Eventz.Add(evett);
                 }
-                foreach (var test in Eventz)
+                MockEventData bb = new MockEventData();
+                var not = await bb.GetNotesAsync(true);
+                foreach(var nott in not)
                 {
-                    // Console.WriteLine(test.StartTime.TimeOfDay);
+                    notes.Add(nott);
                 }
+           
             }
             catch (Exception ex)
             {
