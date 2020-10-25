41007d322d1af9cf4c34d836280f19c4bb909ed0
duylhng <63448508+duylhng@users.noreply.github.com>
2020-10-25 15:59:53 +1000

    Implemented log in with mock users database and error handling

--- src/DAYLY/DAYLY/DAYLY/ViewModels/ProfileViewModel.cs
+++ src/DAYLY/DAYLY/DAYLY/ViewModels/ProfileViewModel.cs
@@ -5,6 +5,10 @@ using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 using DAYLY.Views;
 using DAYLY.Models;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DAYLY.ViewModels
 {
@@ -23,10 +27,19 @@ namespace DAYLY.ViewModels
         public string _CourseHeader;
         public string _LoggedInVisible;
         public string _LoggedOutVisible;
+        public string _LoginEmail;
+        public string _LoginPassword;
+        public string _MessageVisible;
+        public string _MessageText;
+        public string _MessageTextColour;
+        public string _MessageBorderColour;
         string[] oldUserData;
         public ICommand EditProfileCommand { get; }
         public ICommand ConfirmNewProfileDataCommand { get; }
         public ICommand CancelEditProfileCommand { get; }
+        public ICommand LoginCommand { get; }
+        public ICommand LoginCommandMain { get; }
+        public ICommand SignupCommand { get; }
 
         public string FullName
         {
@@ -184,11 +197,91 @@ namespace DAYLY.ViewModels
             }
         }
 
+        public string LoginEmail
+        {
+            get
+            {
+                return _LoginEmail;
+            }
+            set
+            {
+                _LoginEmail = value;
+                OnPropertyChanged(nameof(LoginEmail));
+            }
+        }
+
+        public string LoginPassword
+        {
+            get
+            {
+                return _LoginPassword;
+            }
+            set
+            {
+                _LoginPassword = value;
+                OnPropertyChanged(nameof(LoginPassword));
+            }
+        }
+
+        public string MessageVisible
+        {
+            get
+            {
+                return _MessageVisible;
+            }
+            set
+            {
+                _MessageVisible = value;
+                OnPropertyChanged(nameof(MessageVisible));
+            }
+        }
+
+        public string MessageText
+        {
+            get
+            {
+                return _MessageText;
+            }
+            set
+            {
+                _MessageText = value;
+                OnPropertyChanged(nameof(MessageText));
+            }
+        }
+
+        public string MessageTextColour
+        {
+            get
+            {
+                return _MessageTextColour;
+            }
+            set
+            {
+                _MessageTextColour = value;
+                OnPropertyChanged(nameof(MessageTextColour));
+            }
+        }
+
+        public string MessageBorderColour
+        {
+            get
+            {
+                return _MessageBorderColour;
+            }
+            set
+            {
+                _MessageBorderColour = value;
+                OnPropertyChanged(nameof(MessageBorderColour));
+            }
+        }
+
         public ProfileViewModel()
         {
             EditProfileCommand = new Command(editProfile);
-            ConfirmNewProfileDataCommand = new Command(async () => await NavStack.PopModalAsync());
+            ConfirmNewProfileDataCommand = new Command(confirmNewProfileData);
             CancelEditProfileCommand = new Command(cancelEditProfile);
+            LoginCommand = new Command(async () => await NavStack.PushModalAsync(new Profile_Login()));
+            LoginCommandMain = new Command(login);
         }
 
         async void editProfile()
@@ -205,6 +298,90 @@ namespace DAYLY.ViewModels
             Course = oldUserData[3];
             await NavStack.PopModalAsync();
         }
+
+        async void confirmNewProfileData()
+        {
+            SQLiteConnection conn = DependencyService.Get<Isqlite>().GetConnection();
+
+            string[] newUserData = new string[] { FullName, Email, Organisation, Course };
+
+            if (!isDataSame(oldUserData, newUserData))
+            {
+                if (newUserData[1] != oldUserData[1]) // if new email is different
+                {
+                    List<UserProfile> existingUser = conn.Query<UserProfile>("SELECT * FROM UserProfile WHERE Email=?", newUserData[1]);
+
+                    if (existingUser.Count() == 0) // if new email is not in the database
+                    {
+                        conn.Query<UserProfile>("UPDATE UserProfile SET FullName=?, Email=?, Organisation=?, Course=? WHERE Email=?",
+                                            FullName, Email, Organisation, Course, Email);
+                        showMessage("True", "Successfully updated your profile!", "#228B22", "#228B22");
+                        await Task.Delay(1000);
+                        await NavStack.PopModalAsync();
+                        MessageVisible = "False";
+                    }
+                    else // if new email already in database
+                    {
+                        showMessage("True", "This email belongs to another user.", "#FF0000", "#FF0000");
+                    }
+                }
+                else // if email is the same but other fields are different
+                {
+                    conn.Query<UserProfile>("UPDATE UserProfile SET FullName=?, Organisation=?, Course=? WHERE Email=?",
+                                            FullName, Organisation, Course, Email);
+                    showMessage("True" ,"Successfully updated your profile!", "#228B22", "#228B22");
+                    await Task.Delay(1000);
+                    await NavStack.PopModalAsync();
+                    MessageVisible = "False";
+                }
+            }
+            else
+            {
+                await NavStack.PopModalAsync();
+            }
+
+            conn.Close();   
+        }
+
+        async void login()
+        {
+            SQLiteConnection conn = DependencyService.Get<Isqlite>().GetConnection();
+
+            List<UserProfile> existingUser = conn.Query<UserProfile>("SELECT * FROM UserProfile WHERE Email=?", LoginEmail);
+
+            if (existingUser.Count() == 1)
+            {
+                if (existingUser[0].Password == LoginPassword)
+                {
+                    FullName = existingUser[0].FullName;
+                    Email = existingUser[0].Email;
+                    NumTasks = existingUser[0].TasksRemaining.ToString();
+                    NumTasksCompleted = existingUser[0].TasksCompleted.ToString();
+                    DaysSinceAccountCreated = existingUser[0].DaysSinceAccountCreated.ToString();
+                    Organisation = existingUser[0].Organisation;
+                    Course = existingUser[0].Course;
+                    ProfilePicPath = "profile_pic.png";
+                    LoggedInVisible = "True";
+                    LoggedOutVisible = "False";
+                    showMessage("True", "Success!", "#228B22", "#228B22");
+                    Settings_Main.settingsViewModel.ProfilePicPath = "profile_pic.png";
+                    Settings_Main.settingsViewModel.LogInOutBtnText = "LOG OUT";
+                    await Task.Delay(1000);
+                    await NavStack.PopModalAsync();
+                    MessageVisible = "False";
+                }
+                else
+                {
+                    showMessage("True", "The password you entered is incorrect.", "#FF0000", "#FF0000");
+                }
+            }
+            else
+            {
+                showMessage("True", "The email you entered is incorrect.", "#FF0000", "#FF0000");
+            }
+
+            conn.Close();
+        }
             
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -217,29 +394,9 @@ namespace DAYLY.ViewModels
         {
             NavStack = navigation;
 
-            UserProfile user = new UserProfile
-            {
-                FullName = "John Smith",
-                Email = "johnsmith@email.com",
-                TasksRemaining = 24,
-                TasksCompleted = 15,
-                DaysSinceAccountCreated = 214,
-                Organisation = "Queensland University of Technology",
-                Course = "Information Technology"
-            };
-
-            FullName = user.FullName;
-            Email = user.Email;
-            NumTasks = user.TasksRemaining.ToString();
-            NumTasksCompleted = user.TasksCompleted.ToString();
-            DaysSinceAccountCreated = user.DaysSinceAccountCreated.ToString();
-            Organisation = user.Organisation;
-            Course = user.Course;
-            ProfilePicPath = "profile_pic.png";
-            OrganisationHeader = "Organisation";
-            CourseHeader = "Course";
             LoggedInVisible = "False";
             LoggedOutVisible = "True";
+            MessageVisible = "False";
         }
 
         private bool isDataSame(string[] firstArray, string[] secondArray)
@@ -260,5 +417,12 @@ namespace DAYLY.ViewModels
             return true;
         }
 
+        private void showMessage(string visible, string text, string textColour, string borderColour)
+        {
+            MessageVisible = "True";
+            MessageText = text;
+            MessageTextColour = textColour;
+            MessageBorderColour = borderColour;
+        }
     }
 }
---
+++ src/DAYLY/DAYLY/DAYLY/Views/Profile_Login.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using DAYLY.Views;
+
+using Xamarin.Forms;
+
+namespace DAYLY.Views
+{
+    public partial class Profile_Login : ContentPage
+    {
+        public Profile_Login()
+        {
+            InitializeComponent();
+            BindingContext = Profile.profileViewModel;
+        }
+    }
+}
