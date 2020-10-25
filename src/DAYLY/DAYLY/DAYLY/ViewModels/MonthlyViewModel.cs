499716352ce421480dc624adb10385723e53e679
Ethan-gif <62533979+Ethan-gif@users.noreply.github.com>
2020-10-25 11:22:48 +1000

    hooked monthly view up to database
    hooked mohtly view up added some more descriptions to each event. Removed code from code behind

--- src/DAYLY/DAYLY/DAYLY/ViewModels/MonthlyViewModel.cs
+++ src/DAYLY/DAYLY/DAYLY/ViewModels/MonthlyViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using DAYLY.Models;
 using DAYLY.Services;
+using DAYLY.Views;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,31 +9,79 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 using Xamarin.Plugin.Calendar.Models;
 
 namespace DAYLY.ViewModels
 {
-    //this view model also uses testdata can be altered in mockeventdata
+    //When creating an event to have it displayed on the monthly view it must have a location,name and note
+    //To refresh the monthly view for when new events are added you must press monthly again
+    //unfortunatley couldnt get custom colours working on the monthly view as the package used wasn't flexible enough
     class MonthlyViewModel : BaseViewModel
     {
         private List<Event> zz;
+        private SQLiteConnection conn;
+        private List<Event> eventlist;
+        private List<Programme> colourlist;
+        private List<Note> notelist;
+        private List<Location> localist;
 
         public EventCollection Events { get; set; }
           public ObservableCollection<Event> Eventz { get; }
         public ObservableCollection<Note> notes { get; }
+        public ObservableCollection<Location> locations { get; }
         public MonthlyViewModel()
         {
+            conn = DependencyService.Get<Isqlite>().GetConnection();
+            eventlist = conn.Table<Event>().ToList();
+            colourlist = conn.Table<Programme>().ToList();
+            notelist = conn.Table<Note>().ToList();
+            localist = conn.Table<Location>().ToList();
             //Task.Run(async () => await ExecuteLoadItemsCommand());
-           
-            Eventz = new ObservableCollection<Event>();
+
+            Eventz = new ObservableCollection<Event>(eventlist);
             //   Console.WriteLine("below");
             //   Console.WriteLine(Eventz[1].Date);
-            notes = new ObservableCollection<Note>();
+            locations = new ObservableCollection<Location>(localist);
+            notes = new ObservableCollection<Note>(notelist);
             Events = new EventCollection();
             Task.Run(async () => await Populate());
 
         }
-          async Task Populate()
+        public Command MonthlyCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await Application.Current.MainPage.Navigation.PushAsync(new Monthly());
+                });
+
+            }
+        }
+        public Command DailyCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await Application.Current.MainPage.Navigation.PushAsync(new Daily());
+                });
+
+            }
+        }
+        public Command WeeklyCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await Application.Current.MainPage.Navigation.PushAsync(new Weekly());
+                });
+
+            }
+        }
+        async Task Populate()
         {
             IsBusy = true;
          
@@ -39,6 +89,7 @@ namespace DAYLY.ViewModels
            
                // Console.WriteLine(Eventz.ToString());
                 int index = new int();
+                int locindex = new int();
                 for (int i = 0; i < Eventz.Count; i++) { //loop through how many events there are create a event list each time
                     Console.WriteLine("below me"); //this list is then added to the event collection which is bound to the calendar
                     zz = new List<Event>();
@@ -49,9 +100,17 @@ namespace DAYLY.ViewModels
                             index = notes.IndexOf(not);
                         }
                     }
+                    foreach (var local in locations)
+                    {
+                        if (local.Id == Eventz[i].NoteId)//find the index of the note that corresponds to the event
+                        {
+                            locindex = locations.IndexOf(local);
+                        }
+                    }
                     var temp = new List<Event>(Eventz);//create a temporary list to iterate through so the main list can be modified
-                    zz.Add(new Event { Name =temp[i].Name, Type = notes[index].Description });
+                    zz.Add(new Event { Name =temp[i].Name, Type = notes[index].Description, StartTime = temp[i].StartTime, EndTime = temp[i].EndTime, AlertInterval ="Location: "+locations[locindex].Alias });
                     foreach (var eventt in temp) {
+                   
                         if (eventt.Date == temp[i].Date&&eventt.Name!=temp[i].Name) {
                             foreach (var x in notes) //multiple events on the same date are found then removed
                             {
@@ -60,7 +119,7 @@ namespace DAYLY.ViewModels
                                     index = notes.IndexOf(x);
                                 }
                             }
-                            zz.Add(new Event { Name = eventt.Name, Type = notes[index].Description });
+                            zz.Add(new Event { Name = eventt.Name, Type = notes[index].Description, StartTime=eventt.StartTime,EndTime=eventt.EndTime, AlertInterval=locations[locindex].Alias});
                             //Console.WriteLine(eventt.Name);
                             Eventz.Remove(eventt);
                         }
@@ -101,7 +160,7 @@ namespace DAYLY.ViewModels
                     Eventz.Add(evett);
                 }
                 MockEventData bb = new MockEventData();
-                //var not = await bb.GetNotesAsync(true);
+                var not = await bb.GetNotesAsync(true);
                 foreach(var nott in not)
                 {
                     notes.Add(nott);
